4b3a6eb64019c70cae3cb9f52c3425c9a8542653
song <1044241610@qq.com>
2021-12-03 15:53:16 -0600

    add method that makes EM convergence within delta

--- EMfunctions.py
+++ EMfunctions.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 class spectralEM:
-    def __init__(self, init_mu, labels, max_iter=10):
+    def __init__(self, init_mu, labels):
+        """
+        class initialization
+        :param init_mu: initialization mu, mu is confusion matrix with dimension M workers, K labels, K labels
+        :param labels: crowed sourcing labels result, dimension M workers, N items
+        """
         self.mu = init_mu
         self.M, self.N = labels.shape
         self.K = np.max(labels)+1
         # self.N = num_item
         # self.M = num_worker
         # self.K = num_category
-        self.max_iter = max_iter
         self.q = np.zeros((self.N, self.K)) # / self.K
         self.labels = labels
 
@@ -31,12 +35,22 @@ class spectralEM:
                     self.mu[i, l, c] = np.sum(self.q[:, l] * (self.labels[i, :] == c))
                 self.mu[i, l, :] = self.mu[i, l, :] / np.sum(self.mu[i, l, :])
 
-    def run(self):
-        while self.max_iter > 0:
-            self.E_step()
-            self.M_step()
-            self.max_iter -= 1
+    def run(self, strategy='max_iter', max_iter=10, delta=1e-6):
         self.E_step()
+        d = 1
+        mu0 = self.mu.copy()
+        num_iter = 0
+        while (strategy=='max_iter' and num_iter < max_iter) or (strategy=='converge' and d > delta):
+            self.M_step()
+            self.E_step()
+            num_iter += 1
+            if strategy == 'converge' :
+                mu = self.mu.copy()
+                d = np.sum(np.abs(mu - mu0)) / self.N
+                mu0 = mu
+
+        print('# iterations = ', num_iter)
+
 
     def output_mu(self):
         return self.mu
--- testfunc.py
+++ testfunc.py
@@ -4,16 +4,16 @@ from utils import transform_data, get_confusion_matrix, errorRate
 from EMfunctions import spectralEM
 
 
-df = np.loadtxt('data/bluebird_crowd.txt')
+df = np.loadtxt('data/rte_crowd.txt')
 df = transform_data(df)
 
-truth = np.loadtxt('data/bluebird_truth.txt')
+truth = np.loadtxt('data/rte_truth.txt')
 
 init_mu = get_confusion_matrix(k=2, labels=df)
 # print(init_mu)
 
 EM_optimizor = spectralEM(init_mu=init_mu, labels=df)
-EM_optimizor.run()
+EM_optimizor.run(strategy='max_iter', max_iter=10)
 # print(EM_optimizor.output_mu())
 # print(EM_optimizor.output_q())
 
